ff5e682dd0d14791e18e0d64a1fc2dcc5b4e5dc1
codytheking <codytheking93@gmail.com>
2021-08-18 18:11:02 -0700

    Add starter file

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+/*
+ * This is a comment.
+ *
+ * This is Java code.
+ *
+ * @author Cody King
+ * @date August 19, 2021
+ *
+ */
+ 
+public class Main {
+   public static void main(String[] args) {
+       
+        System.out.println("Hello World!");
+
+        // code...     
+   }
+ }
+ 
+// another comment
+   
